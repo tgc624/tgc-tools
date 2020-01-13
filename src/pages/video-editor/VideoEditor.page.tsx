def931f7095758ce0466361fe11dec1dbc9ba1be
tgc624 <fummy.tgc@gmail.com>
2020-01-13 23:29:23 +0900

    動画のアップロード⇢描画

--- src/pages/video-editor/VideoEditor.page.tsx
+++ src/pages/video-editor/VideoEditor.page.tsx
@@ -1,7 +1,50 @@
 import React from "react";
+// 参考https://blog.vivita.io/entry/2019/04/08/070000
+const VideoUploader = ({ setFileSrc }: any) => {
+  const onLoad = (setSrc: (arg: any) => void) => (event: any) => {
+    const src = event.target?.result;
+    if (src) {
+      setSrc(src);
+    }
+  };
+  const onChange = (setSrc: (arg: any) => void) => (event: any) => {
+    event.persist();
+    const file = event?.target?.files?.[0];
+    if (!file) {
+      return;
+    }
+    const reader = new FileReader();
+    reader.onload = onLoad(setSrc);
+    reader.readAsDataURL(file);
+  };
+  return (
+    <section>
+      <input type="file" onChange={onChange(setFileSrc)} />
+    </section>
+  );
+};
+const VideoViewer = ({ src }: { src?: string }) => {
+  if (!src) {
+    return <section></section>;
+  }
+  return (
+    <section>
+      <video controls src={src} width="100%"></video>
+    </section>
+  );
+};
 
 const VideoEditor: React.FC = () => {
-  return <div>video editor</div>;
+  const [fileSrc, setFileSrc] = React.useState("");
+  return (
+    <article>
+      <header>
+        <h1>動画編集</h1>
+      </header>
+      <VideoUploader setFileSrc={setFileSrc} />
+      <VideoViewer src={fileSrc} />
+    </article>
+  );
 };
 
 export default VideoEditor;
