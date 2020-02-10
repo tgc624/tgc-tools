3f138ae39302a1837cc168517b7d3a80caa7f848
tgc624 <fummy.tgc@gmail.com>
2020-02-11 05:59:52 +0900

    アプリを分割

--- alcohol/src/App.tsx
+++ alcohol/src/App.tsx
@@ -1,42 +1,6 @@
 import React from "react";
 import AlcoholPage from "./pages/Alcohol.page";
-import VideoEditorPage from "./pages/video-editor/VideoEditor.page";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-const App: React.FC = () => {
-  const pages = [
-    { path: "/alcohol", name: "Alcohol", component: <AlcoholPage /> },
-    {
-      path: "/video-editor",
-      name: "Video editor",
-      component: <VideoEditorPage />
-    }
-  ];
-  const Navi = (
-    <nav>
-      <ul>
-        {pages.map((page, index) => {
-          return (
-            <li key={index}>
-              <Link to={page.path}>{page.name}</Link>
-            </li>
-          );
-        })}
-      </ul>
-    </nav>
-  );
-  return (
-    <Router>
-      <Switch>
-        {pages.map((page, index) => (
-          <Route key={index} path={page.path}>
-            {page.component}
-          </Route>
-        ))}
-        <Route path="*">{Navi}</Route>
-      </Switch>
-    </Router>
-  );
-};
+const App: React.FC = () => <AlcoholPage />;
 
 export default App;
---
+++ video-editor/src/App.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import VideoEditorPage from "./pages/VideoEditor.page";
+
+const App: React.FC = () => <VideoEditorPage />;
+
+export default App;
