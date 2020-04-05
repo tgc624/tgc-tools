41be0ed69228456fddeeff96dd596c8de984b91f
tgc624 <fummy.tgc@gmail.com>
2020-04-05 15:20:19 +0900

    neumorphismことはじめ

--- neumahjong/src/App.tsx
+++ neumahjong/src/App.tsx
@@ -3,7 +3,13 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div>neumahjong</div>;
+  return (
+    <div className="app">
+      <div className="box convex full-width">
+        <p className="pa-3">a</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
